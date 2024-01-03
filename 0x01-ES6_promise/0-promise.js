033ffb4d810e55d96a48de1e680e11b1a15b7fe2
l11EG4 <zakariaberahmi@gmail.com>
2024-01-02 19:07:48 -0800

    first part

---
+++ 0x01-ES6_promise/0-promise.js
@@ -0,0 +1,7 @@
+/* eslint-disable no-unused-vars */
+/* made by MEGA */
+export default function getResponseFromAPI() {
+  return new Promise((resolve, reject) => {
+  });
+}
+/* eslint-disable no-unused-vars */
