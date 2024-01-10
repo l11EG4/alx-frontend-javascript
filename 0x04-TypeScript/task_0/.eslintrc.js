22d2f0f70986fe0fdd1319c42bd4bd1bf7de7199
l11EG4 <zakariaberahmi@gmail.com>
2024-01-09 21:59:37 -0800

    task

---
+++ 0x04-TypeScript/task_0/.eslintrc.js
@@ -0,0 +1,13 @@
+/* Made by MEGA */
+module.exports =  {
+  parser:  '@typescript-eslint/parser',
+  extends:  [
+    'plugin:@typescript-eslint/recommended',  // Uses the recommended rules from @typescript-eslint/eslint-plugin
+  ],
+  parserOptions:  {
+    ecmaVersion:  2018,
+    sourceType:  'module',
+  },
+  rules:  {
+  },
+};
